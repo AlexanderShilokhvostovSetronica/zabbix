7608a640fb2001acc73b3d5614304aef8171e50c
Alexander Shilokhvostov <alex@shilli.ru>
2015-09-29 18:23:25 +0600

    Add example alert script file

--- run_zabbix.sh
+++ run_zabbix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 10051:10051 -p 10052:10052 -p 80:80 -p 2812:2812 -d -v /opt/mysql_zabbix:/var/lib/mysql ashilokhvostov/zabbix:latest
+docker run -p 10051:10051 -p 10052:10052 -p 81:80 -p 2812:2812 -d -v /srv/data/docker/zabbix/docker_data:/var/lib/mysql -v /srv/data/git/zabbix/alert_script:/usr/lib/zabbix/alertscripts ashilokhvostov/zabbix:latest
